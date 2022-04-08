7beb55c0b6e3dbefcc6382819b678b826b9438bb
Victoria Llorca Dapena <victoria.llorca@FVFGV0WCQ05Q.local>
2022-04-08 13:30:57 +0200

    Finish web component

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # dile-product-card
-Basic product card for a e-commerce
+Basic product card for a e-commerce based on Lit Element
+
+# Basic card (Responsive)
+You are able to add an image and/or personalize your own text.
+
+## How to use?
+
+Change the value you want to replace using the variables.
+
+## Links
+
+
+
+## Preview
+
+[!(img/Demo-Dile-Product-demo.png)](img/Demo-Dile-Product-demo.png )
+
+![image info](Demo-Dile-Product-demo.png)
+
+
+## Variables:
+
+### Text
+- "main_title"
+- "img_src"
+- "img_alt"
+- "product_title"
+- "product_subtitle"
+- "product_text"
+- "product_price"
+- "btn_text"
+
+### CSS
+
+### Mobile and desktop
+
+ -  --color-text: Title and text color
+-   --color-subtext: Subtitle and price color
+-   --color-bg-first: Bg color of image
+-   --color-bg-second: Bg color of text
+-   --color-border: Border color
+  
+  ### Mobile
+  
+-  --font-opacity: Subtitle opacity
+-   --font-main-title: Title size
+-   --font-title: Title size (Behind image)
+-   --font-subtitle: Subtitle size
+-   --font-text: Text size
+-   --font-price: Price size
+-   --font-btn: Button size
+  
+  ### Desktop
+  
+-   --fontDS-main-title:  Title size
+-   --fontDS-title:  Title size (Behind image)
+-   --fontDS-subtitle: Subtitle size
+-   --fontDS-text: Text size
+-   --fontDS-price: Price size
+-   --fontDS-btn: Button size
+  
+##   Author
+
+
+
+## License
+
+Under MIT License
--- dile-product-card.js
+++ dile-product-card.js
@@ -5,7 +5,7 @@ class DileProductCard extends HTMLElement {
     }
   
     static get observedAttributes() {
-      return ["main_title", "img_src", "img_alt", "product_title", "product_subtitle", "product_text", "product_price", "btn_text"];
+      return ["img_src", "img_alt", "product_title", "link_producto", "product_text", "product_price", "btn_text"];
     }
     attributeChangedCallback(name, oldValue, newValue) {
       this[name] = newValue;
@@ -15,20 +15,18 @@ class DileProductCard extends HTMLElement {
       const template = document.createElement("template");
       template.innerHTML = /*html*/ `
   
-      <section class="container">
-        <div class="container__first">
-          <h1 class="container__first--title">${this.main_title}</h1>
-          <img src="${this.img_src}" alt="${this.img_alt}" class="container__first--image">
+      <section class="product-item">
+        <div class="product-item__first">
+          <img src="${this.img_src}" alt="${this.img_alt}" class="product-item__first--image">
         </div>
-        <div class="container__second">
-          <div class="container__second__texts" >
-            <h2 class="container__second__texts--title">${this.product_title}</h2>
-            <h3 class="container__second__texts--subtitle">${this.product_subtitle}</h3>
+        <div class="product-item__second">
+          <div class="product-item__second__texts" >
+            <h2 class="product-item__second__texts--title"><a href="${this.link_producto}">${this.product_title}</a></h2>
           </div>
-          <p class="container__second--text">${this.product_text}</p>
-          <div class="container__second__details">
-            <h2 class="container__second__details--price">${this.product_price}</h2>
-            <button class="container__second__details--btn">${this.btn_text}</button>
+          <p class="product-item__second--text">${this.product_text}</p>
+          <div class="product-item__second__details">
+            <h2 class="product-item__second__details--price">${this.product_price}</h2>
+            <button href="${this.link_producto}" class="product-item__second__details--btn">${this.btn_text}</button>
           </div>
         </div>
       </section>
@@ -41,7 +39,7 @@ class DileProductCard extends HTMLElement {
     getStyles() {
       return `
       <style>
-      @import "dile-product-card.css"
+      @import "../dile-product-card.css"
       </style>
       `
     }
